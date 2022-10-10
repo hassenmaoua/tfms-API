9776fe3cb638fc3b6d648537bbe1aca5c47276cd
Hassen Maoua <hassenmaoua07@gmail.com>
2022-10-10 22:21:55 +0100

    heroku commit

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const app = require('./app');
 
 app.listen(port, () => {
   console.log('You can use lathe-mill-workshop API in your APP.\n');
-  process.env.API_PORT && console.log(`\tLocal: http://127.0.0.1:${port}\n`);
+  console.log(`\tLocal: http://127.0.0.1:${port}\n`);
 });
